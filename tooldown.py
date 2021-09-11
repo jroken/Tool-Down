6892392f94a1dc96c78bf08247dfd2ad1a0d1796
jroken <83027391+jroken@users.noreply.github.com>
2021-09-11 11:48:18 +0300

    Update and rename downpload.py to tooldown.py

--- tooldown.py
+++ tooldown.py
@@ -6,7 +6,7 @@ print("""
 =========================================================================
 |                            CODED BY JROKEN                            |
 |                                                                       |
-|                               DOWNPLOAD                               |
+|                               TOOLDOWN                                |
 |                                                                       |
 |           Telegram          t.me/hakoo29                              |
 |                                                                       |
